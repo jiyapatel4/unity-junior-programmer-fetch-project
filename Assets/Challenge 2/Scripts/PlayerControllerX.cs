4686cad02ac023e1a61690669e83280a4a94548f
Jiya Patel <128255337+jiyapatel4@users.noreply.github.com>
2023-05-16 16:32:15 -0400

    Fixed bugs and added bonus features
    Bugs Fixed:
    - Proper objects spawn
    - Collision detection is more targeted
    
    Bonus feature:
    - Player has a cooldown

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,12 +6,19 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    // dog cooldown times
+    private float nextFire = 0.0f;
+    private float fireRate = 0.5f;
+
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
         {
+            // update nextFire every time the space key is pressed
+            nextFire += fireRate;
+
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
